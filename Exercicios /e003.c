fbe019dd3fa764669589d813ab36116edb7533b4
Laiz Detros <megami.ay@gmail.com>
2024-08-13 21:18:27 -0300

    Create e003.c

---
+++ Exercicios /e003.c
@@ -0,0 +1,5 @@
+// Construa um programa que define um tipo novo de dados, para uma estrutura de um animal,
+// deverá conter atributos como: raça, cor, peso e sexo.  
+// E solicite ao usuário do programa 10 espécies e depois imprima em tela.  
+#include <stdio.h>
+#include <stdlib.h>
