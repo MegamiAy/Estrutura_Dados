212dae97459af3dc2b6ac4eedcf2a95acb2f352c
Laiz Detros <megami.ay@gmail.com>
2024-08-20 19:30:53 -0300

    Update e003.c

--- Exercicios /e003.c
+++ Exercicios /e003.c
@@ -1,5 +1,8 @@
-// Construa um programa que define um tipo novo de dados, para uma estrutura de um animal,
-// deverá conter atributos como: raça, cor, peso e sexo.  
-// E solicite ao usuário do programa 10 espécies e depois imprima em tela.  
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct 
+
+int main(){
+
+}
